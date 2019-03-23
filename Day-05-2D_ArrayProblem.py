97c436c5630232c0eeaddd49b0b0130889c39a67
Louisa <46016065+lulets@users.noreply.github.com>
2019-03-23 20:36:14 +0400

    Create Day-05-2D_ArrayProblem.py
    StringChallenge

---
+++ Day-05-2D_ArrayProblem.py
@@ -0,0 +1,109 @@
+"""
+Context 
+Given a 6 X 6 2D Array, A:
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+
+We define an hourglass in A to be a subset of values with indices falling in this pattern in A's graphical representation:
+
+a b c
+  d
+e f g
+
+There are  hourglasses in A, and an hourglass sum is the sum of an hourglass' values.
+
+
+Task:
+Calculate the hourglass sum for every hourglass in A,then print the maximum hourglass sum.
+
+Input Format:
+There are 16 lines of input, where each line contains  space-separated integers describing 2D Array A; every value in A will be in the inclusive range of -9 to 9.
+
+Constraints:
+-9 <= A[i][j] <= 9
+0 <= i,j <= 5
+
+Output Format:
+Print the largest (maximum) hourglass sum found in A.
+
+Sample Input:
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 2 4 4 0
+0 0 0 2 0 0
+0 0 1 2 4 0
+
+Sample Output:
+19
+
+
+Explanation:
+A contains the following hourglasses:
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  1       0       0       0
+1 1 1   1 1 0   1 0 0   0 0 0
+
+0 1 0   1 0 0   0 0 0   0 0 0
+  1       1       0       0
+0 0 2   0 2 4   2 4 4   4 4 0
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  0       2       4       4
+0 0 0   0 0 2   0 2 0   2 0 0
+
+0 0 2   0 2 4   2 4 4   4 4 0
+  0       0       2       0
+0 0 1   0 1 2   1 2 4   2 4 0
+
+The hourglass with the maximum sum (19) is:
+2 4 4
+  2
+1 2 4
+
+
+Sample Input 2:
+-1 -1 0 -9 -2 -2
+-2 -1 -6 -8 -2 -5
+-1 -1 -1 -2 -3 -4
+-1 -9 -2 -4 -4 -5
+-7 -3 -3 -2 -9 -9
+-1 -3 -1 -2 -4 -5
+
+
+Expected answer:
+-6
+
+Consider there are negative numbers
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+    
+    max_sum = 0 - math.inf
+    for i in range(int(len(arr) / 2) + 1):
+        for j in range(int(len(arr) / 2) + 1):
+            curr_sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+            if curr_sum > max_sum:
+                max_sum = curr_sum
+    
+    print (max_sum)
+
