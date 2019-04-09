5cea95692f474db166bcb360f836ac852288c6b4
lulets-l <46016065+lulets@users.noreply.github.com>
2019-04-09 22:14:28 +0400

    update to task description

--- Day-09-LinkedLists.py
+++ Day-09-LinkedLists.py
@@ -1,32 +1,33 @@
-"""
-Day 09 - Linked Lists - HackerRank
+""" Day 09 - Linked Lists - HackerRank
 
-Task 
-Complete the insert function in your editor so that it creates a new Node (pass data as the Node constructor argument) and inserts it at the tail of the linked list referenced by the head parameter. Once the new node is added, return the reference to the head node.
+Task  Complete the insert function in your editor so that it creates a
+new Node (pass data as the Node constructor argument) and inserts it
+at the tail of the linked list referenced by the head parameter. Once
+the new node is added, return the reference to the head node.
 
-Note: If the head argument passed to the insert function is null, then the initial list is empty.
+Note: If the head argument passed to the insert function is null, then
+ the initial list is empty.
 
-Input Format:
-The insert function has 2 parameters: a pointer to a Node named head, and an integer value, data. The constructor for Node has 1 parameter: an integer value for the data field. You do not need to read anything from stdin.
+Input Format: The insert function has 2 parameters: a pointer to a
+Node named head,  and an integer value, data.  The constructor for
+Node has 1 parameter: an integer value for the data field. You do not
+need to read anything from stdin.
 
-Output Format:
-Your insert function should return a reference to the head node of the linked list.
+Output Format: Your insert function should return a reference to the
+head node of the linked list.
 
-Sample Input:
-The first line contains T, the number of test cases. 
-The T subsequent lines of test cases each contain an integer to be inserted at the list's tail.
-4
-2
-3
-4
-1
+Sample Input: The first line contains T, the number of test cases. 
+The T subsequent lines of test cases each contain an integer to be
+inserted at the list's tail. 4 2 3 4 1
 
-Sample Output:
-The display method handles the output.
-2 3 4 1
+Sample Output: The display method handles the output. 2 3 4 1
 
-Explanation:
-T = 4, so 4 nodes should be inserted. The list is initially empty, so head is null; accounting for this, our code returns a new node containing the data value 2 as the head of our list. We then create and insert nodes 3, 4, and 1 at the tail of our list. The resulting list returned by the last call to insert is [2,3,4,1], so the printed output is 2 3 4 1.
+Explanation: T = 4, so 4 nodes should be inserted. The list is
+initially empty, so head is null; accounting for this, our code
+returns a new node containing the data value 2 as the head of our
+list. We then create and insert nodes 3, 4, and 1 at the tail of our
+list. The resulting list returned by the last call to insert is
+[2,3,4,1], so the printed output is 2 3 4 1. 
 """
 
 class Node:
